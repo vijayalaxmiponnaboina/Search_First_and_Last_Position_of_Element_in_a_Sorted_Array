66a86d33f125380a2e0315820c2d30a38279f660
vijayalaxmiponnaboina <ponnaboinarahulyadav@gmail.com>
2026-05-28 17:18:16 +0530

    Add files via upload

---
+++ SearchRange.py
@@ -0,0 +1,34 @@
+class Solution:
+    def lowerBound(self,nums,target):
+        n=len(nums)
+        l=0
+        r=n-1
+        ans=n
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]>=target:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ans
+    def upperBound(self,nums,target):
+        n=len(nums)
+        l=0
+        r=n-1
+        ans=n
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]>target:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ans
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        lb=self.lowerBound(nums,target)
+        ub=self.upperBound(nums,target)
+        if lb==ub:
+            return [-1,-1]
+        else:
+            return [lb,ub-1]
